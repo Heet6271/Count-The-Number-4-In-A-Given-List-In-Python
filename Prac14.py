9663132c8739e6f4b7d65cc809d73877209dd14f
Heet Thummar <thummarhit6271@gmail.com>
2023-07-12 10:36:41 +0530

    p14

---
+++ Prac14.py
@@ -0,0 +1,17 @@
+'''def listcount(nums):
+    count = 0
+    for i in nums:
+        if i == 4:
+            count = count + 1
+            
+    return count
+
+
+a=[1,2,3,4,5]
+print(listcount([1,4,6,7,4]))
+print(listcount([1,4,6,7,4,5]))
+a=[1,2,3,4,5,4,4]
+print(listcount(a))'''
+
+a=input("Please enter the list:->")
+print(a[0:4])
